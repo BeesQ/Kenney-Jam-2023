638f0ad7e51080609926b66d28e83f9800dd9f3b
Kamil Kozioł <koziol.kamil@protonmail.com>
2023-07-22 19:05:17 +0200

    Added movetowards

---
+++ Assets/Scripts/EnemyBehaviours/MoveTowards.cs
@@ -0,0 +1,30 @@
+using System;
+using UnityEngine;
+
+namespace EnemyBehaviours
+{
+    public class MoveTowards : MonoBehaviour
+    {
+        public Transform moveTo;
+        public float keepingDistanceRange = 5.0f;
+        public float moveSpeed = 5.0f;
+
+        private void Update()
+        {
+
+            if (Vector2.Distance(transform.position, moveTo.position) <= keepingDistanceRange)
+            {
+                Vector3 diff = (moveTo.position - transform.position).normalized;
+                transform.position = Vector2.MoveTowards(transform.position, transform.position - diff, Time.deltaTime * moveSpeed);
+                return;
+            }
+
+            float deadZoneSize = 0.1f;
+            if (Vector2.Distance(transform.position, moveTo.position) > keepingDistanceRange + deadZoneSize)
+            {
+                transform.position =
+                    Vector2.MoveTowards(transform.position, moveTo.position, Time.deltaTime * moveSpeed);
+            }
+        }
+    }
+}
